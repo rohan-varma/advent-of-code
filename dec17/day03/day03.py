1a11041f778c5a40ea9b7d018a925b0b38709a38
Rohan Varma <rvarm1@gmail.com>
2017-12-17 22:25:16 -0800

    day03 day04

---
+++ dec17/day03/day03.py
@@ -0,0 +1,6 @@
+
+def spiral_calculate_distance(num):
+	pass
+	
+if __name__ == '__main__':
+	num = 289326
--- dec17/day04/day04.py
+++ dec17/day04/day04.py
@@ -1,2 +1,15 @@
+def count_anagrams(phrase):
+	sorted_to_occ = {}
+	for x_ in phrase:
+		x = "".join(sorted(x_))
+		if x in sorted_to_occ:
+			sorted_to_occ[x]+=1
+		else:
+			sorted_to_occ[x]=0
+	return sum(sorted_to_occ.values())
+
+def is_valid(phrase):
+	return count_anagrams(phrase) == 0
+
 if __name__ == '__main__':
-	print(sum([1 if len(set(phrase)) == len(phrase) else 0 for phrase in [[s.strip() for s in line.split(" ")] for line in open('input.txt').readlines()]]))
+	print(sum([1 if is_valid(phrase) else 0 for phrase in [[s.strip() for s in line.split(" ")] for line in open('input.txt').readlines()]]))
