fe457f7d867c411124798d217e41672c914942ad
Rohan Varma <rvarm1@gmail.com>
2017-12-23 22:36:46 -0800

    improvement

--- dec17/day06/day06.py
+++ dec17/day06/day06.py
@@ -5,19 +5,25 @@ def repeat_until_cycle(li):
 		if count % 500 == 0:
 			print('count is {}'.format(count))
 		# find the max in li, breaking ties by idx
+		# O(N elements in list) time op
 		max_idx, max_val = max(([idx, val] for idx, val in enumerate(li)), key = lambda l: l[1])
 		# do a redistribution op
 		li[max_idx] = 0
 		num_to_distribute = max_val
+		all_inc_by = num_to_distribute//len(li)
+		li = [val + all_inc_by for val in li]
 		cur_idx = max_idx + 1 if max_idx + 1 < len(li) else 0
-		while num_to_distribute:
-			# cur_idx is going to be valid, distribute 1 here and move on
+		remainder = num_to_distribute % len(li)
+		# O(max(li)) loop
+		while remainder:
+			# distribute 1 to cur_idx
 			li[cur_idx]+=1
-			num_to_distribute-=1
+			remainder-=1
 			cur_idx = cur_idx + 1 if cur_idx + 1 < len(li) else 0
 		# cool, that was one distribution
 		count+=1
 		# now check if we've ever see this arrangement
+		#  O(N) time op (for the hashing)
 		if tuple(li) in seen_with_count:
 			return count, count - seen_with_count[tuple(li)]
 		else:
